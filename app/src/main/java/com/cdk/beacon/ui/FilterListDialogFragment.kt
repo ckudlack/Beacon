afa8ff2fd7df7732430f173ff2419813f5736dc5
Chris Kudlack <chrisk@turo.com>
2018-03-14 17:05:03 -0400

    Fixed layout name

--- app/src/main/java/com/cdk/beacon/ui/FilterListDialogFragment.kt
+++ app/src/main/java/com/cdk/beacon/ui/FilterListDialogFragment.kt
@@ -7,7 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.cdk.beacon.R
-import kotlinx.android.synthetic.main.fragment_trip_list_dialog.*
+import kotlinx.android.synthetic.main.fragment_filter_list_dialog.*
 
 class FilterListDialogFragment : BottomSheetDialogFragment() {
     private var mListener: Listener? = null
